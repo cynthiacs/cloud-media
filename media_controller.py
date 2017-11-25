54aac3abbdf3e61757b55456506aac4ebc07061d
Yang Xudong <alex.xdyang@gmail.com>
2017-11-25 17:24:33 +0800

    deal with "role" in media controller

--- media_controller.py
+++ media_controller.py
@@ -7,7 +7,7 @@ _REQUEST_OFFLINE = 'offline'
 _REQUEST_GET_NODES_ONLINE = 'get_nodes_online'
 
 _TOPIC_NODES_WILL = 'nodes_will/#'
-_TOPIC_NODES_ON_LINE = 'nodes_online'
+_TOPIC_NODES_ON_LINE = 'cm/nodes_online/'  # + role
 
 p2pc = None
 
@@ -33,19 +33,23 @@ class CollectionOnLine(object):
         l_nodes_online = list(nodes_online)
         return str(l_nodes_online)
 
+    def find_role(self, role):
+        nodes_online = self._db_col_nodes_online.find({"role": role})
+        l_nodes_online = list(nodes_online)
+        return str(l_nodes_online)
+
     def publish(self, p2p_client):
+        print("\t  " + self.find())
         p2p_client.mqtt_publish(_TOPIC_NODES_ON_LINE, self.find(), qos=2, retain=True)
 
-    def online(self, p2p_client, whoami, params):
+    def online(self, whoami, params):
+        print("online:" + whoami)
         self.remove(whoami)
         self.insert(params=params)
-        self.publish(p2p_client)
 
-    def offline(self, p2p_client, whoami):
-        if p2p_client is None:
-            raise ValueError("p2p client should not be None")
+    def offline(self, whoami):
+        print("offline:" + whoami)
         self.remove(whoami)
-        self.publish(p2p_client)
 
 
 _online_col = CollectionOnLine()
@@ -63,8 +67,10 @@ def handle_online(jrpc):
     print(repr(jrpc))
     params = jrpc['params']
     whoami = params['whoami']
+    role = params['role']
+    _online_col.online(whoami, params)
 
-    _online_col.online(p2pc, whoami, params)
+    p2pc.mqtt_publish(_TOPIC_NODES_ON_LINE + role, _online_col.find_role(role=role), qos=2, retain=True)
     return "OK"
 
 
@@ -80,15 +86,21 @@ def handle_offline(jrpc):
     print(repr(jrpc))
     params = jrpc['params']
     whoami = params['whoami']
-    _online_col.offline(p2pc, whoami)
+    role = params['role']
+    _online_col.offline(whoami)
+
+    p2pc.mqtt_publish(_TOPIC_NODES_ON_LINE + role, _online_col.find_role(role=role), qos=2, retain=True)
     return "OK"
 
 
 def handle_get_nodes_online(jrpc):
     print(repr(jrpc))
-    # params = jrpc['params']
+    params = jrpc['params']
+    print("the search role is: " + params['role'])
+    print("the search result is:")
+    print(_online_col.find_role(params['role']))
     # whoami = params['whoami']
-    return _online_col.find()
+    return _online_col.find_role(params['role'])
 
 
 def handle_nodes_will(msg):
