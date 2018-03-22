cfea790bffd176398e792ecd9193bf83705d3dc6
yushenqing <caler2005@163.com>
2018-03-22 17:27:20 +0800

    add login_app route for APP debug
    Signed-off-by: yushenqing <caler2005@163.com>

--- http_server/app/auth/views.py
+++ http_server/app/auth/views.py
@@ -5,6 +5,7 @@ from . import auth
 from .. import db
 from ..models import User
 from .forms import LoginForm
+import json
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -18,5 +19,29 @@ def login():
 
         #flash('Not Implement Yet!')
     return render_template('auth/login.html', form=form)
+
+@auth.route('/login_app', methods=['GET', 'POST'])
+def login_app():
+    #assert request.method == 'POST'
+    action = request.args.get('action', None)
+    if action == 'in':
+        #data = request.get_data()
+        #dict_in = json.loads(data)
+        #account = dict_in["account"]
+        #password = dict_in["password"]
+        #print("LOGIN: account:%s, password:%s" %(account, password))
+        dict_return = {"result":"OK", "role":"pusher", "token":"12345678",
+                       "vendor_id":"88888888", "vendor_nick":"CM Team",
+                       "group_id":"00000000", "group_nick":"Default Group"}
+        return json.dumps(dict_return)
+    elif action == 'out':
+        #data = request.get_data()
+        #dict_out = json.loads(data)
+        #account = dict_out["account"]
+        #print("LOGOUT: account:%s" %(account))
+
+        return json.dumps({"result":"OK"})
+    else:
+        return json.dumps({"result":"ERROR"})
 
 
