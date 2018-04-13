14ffb29e73ebe2df1cd9afc33798cd5103637c06
yushenqing <caler2005@163.com>
2018-04-13 15:49:54 +0800

    add app login and logout checking with response from database
    Signed-off-by: yushenqing <caler2005@163.com>

--- http_server/app/auth/views.py
+++ http_server/app/auth/views.py
@@ -23,21 +23,41 @@ def login_app():
     # assert request.method == 'POST'
     action = request.args.get('action', None)
     if action == 'in':
-        # data = request.get_data()
-        # dict_in = json.loads(data)
-        # account = dict_in["account"]
-        # password = dict_in["password"]
-        # print("LOGIN: account:%s, password:%s" %(account, password))
-        dict_return = {"result": "OK", "role": "pusher", "token": "12345678",
-                       "vendor_id": "88888888", "vendor_nick": "CM Team",
-                       "group_id": "00000000", "group_nick": "Default Group"}
+        data = request.get_data()
+        dict_in = json.loads(data)
+        account = dict_in['account']
+        password = dict_in['password']
+        print("LOGIN: account:%s, password:%s" %(account, password))
+
+        cur_user = User.objects(account=account).first()
+        if cur_user is None:
+            return json.dumps({'result': 'ERROR'})
+        if password != cur_user.password:
+            return json.dumps({'result': 'ERROR'})
+        user_info_seq = ('result', 'role', 'token', 'vendor_id', 'vendor_nick', 'group_id', 'group_nick')
+        dict_return = dict.fromkeys(user_info_seq)
+        dict_return['result'] = 'OK'
+        dict_return['role'] = cur_user.role
+        dict_return['token'] = cur_user.token
+        dict_return['vendor_id'] = cur_user.vid
+        dict_return['vendor_nick'] = cur_user.vendor
+        dict_return['group_id'] = cur_user.group.gid
+        dict_return['group_nick'] = cur_user.group.name
+
+        #dict_return = {"result": "OK", "role": "pusher", "token": "12345678",
+        #               "vendor_id": "88888888", "vendor_nick": "CM Team",
+        #               "group_id": "00000000", "group_nick": "Default Group"}
         return json.dumps(dict_return)
     elif action == 'out':
-        # data = request.get_data()
-        # dict_out = json.loads(data)
-        # account = dict_out["account"]
-        # print("LOGOUT: account:%s" %(account))
+        data = request.get_data()
+        dict_out = json.loads(data)
+        account = dict_out["account"]
+        print("LOGOUT: account:%s" %(account))
 
-        return json.dumps({"result": "OK"})
+        cur_user = User.objects(account=account).first()
+        if cur_user is None:
+            return json.dumps({'result': 'ERROR'})
+
+        return json.dumps({'result': 'OK'})
     else:
-        return json.dumps({"result": "ERROR"})
+        return json.dumps({'result': 'ERROR'})
