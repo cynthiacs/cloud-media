ed6a80923befab11386ec871a12c087de37c06c2
user0 <user_wangjianyong@outlook.com>
2018-06-29 15:04:09 +0800

    user manager:   1. revert os environ HOST

--- http_server/config.py
+++ http_server/config.py
@@ -2,12 +2,12 @@ import os
 
 config = {
     "mqtt": {
-        "broker_url": '47.100.125.222',
+        "broker_url": os.environ.get('CM_MQTT_HOST'),
         "broker_port": 1883,
         "connect_timeout_s": 60
     },
     "mongo": {
-        "server_url": '47.100.125.222',
+        "server_url": os.environ.get('CM_MONGO_HOST'),
         "server_port": 27017,
         "db_name": "usermanager"
     },
