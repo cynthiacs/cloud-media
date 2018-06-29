5d2af39d13ac16b77fae28c5589ff32c99ef01d7
user0 <user_wangjianyong@outlook.com>
2018-06-29 14:58:56 +0800

    user manager:   1. start live and stop live button action handler

--- http_server/config.py
+++ http_server/config.py
@@ -1,14 +1,13 @@
 import os
 
-
 config = {
     "mqtt": {
-        "broker_url":os.environ.get('CM_MQTT_HOST'),
+        "broker_url": '47.100.125.222',
         "broker_port": 1883,
         "connect_timeout_s": 60
     },
     "mongo": {
-        "server_url":os.environ.get('CM_MONGO_HOST'),
+        "server_url": '47.100.125.222',
         "server_port": 27017,
         "db_name": "usermanager"
     },
